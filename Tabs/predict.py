08105deb028c19233e00ff49c4fe72fc11be8555
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-08-23 19:28:16 +0530

    Update predict.py

--- Tabs/predict.py
+++ Tabs/predict.py
@@ -56,26 +56,25 @@ def app(df, X, y):
             st.success("The air quality is medium. 😀")
             # st.markdown(
     # f'<a href="https://www.amazon.in/s?k=aquariums&ref=nb_sb_noss_1" target="_blank" style="display: inline-block; padding: 12px 20px; background-color: green; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 4px;">Order an Aquarium at the best price now!</a>',
-    # unsafe_allow_html=True
-)
+    # unsafe_allow_html=True)
+
         elif (prediction == 3):
             st.error("The air quality is poor and unhealthy. 😐🍃")
             #st.markdown(
     # f'<a href="https://www.amazon.in/s?k=less+pollution+zone+air+purifier&rh=n%3A976442031%2Cp_n_pct-off-with-tax%3A2665400031&dc&ds=v1%3AhSeUdGPbxL0p9rWpgB4FzQxSdQ3NRR3fQs%2BnDBFeenY&rnid=2665398031&ref=sr_nr_p_n_pct-off-with-tax_2" target="_blank" style="display: inline-block; padding: 12px 20px; background-color: orange; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 4px;">Order an Air Purifier at the best price now!</a>',
-    # unsafe_allow_html=True
-)
+    # unsafe_allow_html=True)
+
         elif (prediction == 4):
             st.error("The air quality is very unhealthy. 😫❗")
            #  st.markdown(
     # f'<a href="https://www.amazon.in/s?k=powerful+air+purifier&i=kitchen&ref=nb_sb_noss" target="_blank" style="display: inline-block; padding: 12px 20px; background-color: brown; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 4px;">Order an Air Purifier at the best price now!</a>',
-    # unsafe_allow_html=True
-)
+    # unsafe_allow_html=True)
+
         else:
             st.error("The air quality is hazardous. 😵‍💫")
           #  st.markdown(
     # f'<a href="https://www.amazon.in/s?k=high+pollution+air+purifier&i=kitchen&ref=nb_sb_noss" target="_blank" style="display: inline-block; padding: 12px 20px; background-color: red; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 4px;">Order an Air Purifier at the best price now!</a>',
-    # unsafe_allow_html=True
-) 
+    # unsafe_allow_html=True) 
             
         # Print teh score of the model 
         st.sidebar.write("The model used is trusted by environmetal scientists and has an accuracy of ", (score*100),"%")
